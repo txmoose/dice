896fec16569478bf244cb0cd1df9faf145ec3813
Kyle Brieden <kyle@brieden.org>
2015-04-21 05:08:43 -0500

    Changed dice to an object in preperation for Flask appliction later

--- dice.py
+++ dice.py
@@ -1,24 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import random
 
 
-def roll(crit):
-    return random.randint(1, crit)
+class dice:
+    def __init__(self):
+        pass
 
+    def roll(self, crit):
+        return random.randint(1, crit)
 
-def dice(cnt, crit):
-    faces = [0]
-    for x in range(0, cnt):
-        face = roll(crit)
-        faces[0] += face
-        faces.append(face)
+    def dice(self, cnt, crit):
+        faces = [0]
+        for x in range(0, cnt):
+            face = self.roll(crit)
+            faces[0] += face
+            faces.append(face)
 
-    print("The roll is {}".format(faces[1:]))
+        print("The roll is {}".format(faces[1:]))
 
-    if len(faces) > 2:
-        print("The total is {}".format(faces[0]))
+        if len(faces) > 2:
+            print("The total is {}".format(faces[0]))
 
 
 if __name__ == '__main__':
@@ -27,6 +30,8 @@ if __name__ == '__main__':
                         help='Number and Type of Dice in the form NdX')
     args = parser.parse_args()
 
+    Dice = dice()
+
     (cnt, crit) = (args.dice.split('d'))
 
-    dice(int(cnt), int(crit))
+    Dice.dice(int(cnt), int(crit))
