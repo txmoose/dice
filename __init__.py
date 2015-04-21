8cfb2c1f2f0765e41b4865860077b3929c86e31f
Kyle Brieden <kyle@brieden.org>
2015-04-21 09:02:22 -0500

    Added support for multiple dice strings on cmd line and moved parsing of strings to the dice function

--- __init__.py
+++ __init__.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse
-from dice import Dice
-
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Simple dice roller')
-    parser.add_argument('dice', type=str,
-                        help='Number and Type of Dice in the form NdX')
-    args = parser.parse_args()
-
-    dice = Dice()
-
-    (cnt, crit) = (args.dice.split('d'))
-
-    dice.dice(int(cnt), int(crit))
+    import sys
+    main(sys.argv)
--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import random
 
 
@@ -10,14 +11,29 @@ class Dice:
     def roll(self, crit):
         return random.randint(1, crit)
 
-    def dice(self, cnt, crit):
+    def dice(self, die_list):
         faces = [0]
-        for x in range(0, cnt):
-            face = self.roll(crit)
-            faces[0] += face
-            faces.append(face)
+        for x in die_list:
+            (cnt, crit) = x.split('d')
+            (cnt, crit) = (int(cnt), int(crit))
+
+            for y in range(0, cnt):
+                face = self.roll(crit)
+                faces[0] += face
+                faces.append(face)
 
         print("The roll is {}".format(faces[1:]))
 
         if len(faces) > 2:
             print("The total is {}".format(faces[0]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simple dice roller')
+    parser.add_argument('dice', nargs='+',
+                        help='Number and Type of Dice in the form NdX')
+    args = parser.parse_args()
+
+    dice = Dice()
+
+    dice.dice(args.dice)
