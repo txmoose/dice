de1f26742a2eb54827840fb335cb469a31ffad11
Kyle Brieden <kyle@brieden.org>
2015-04-26 05:51:13 -0500

    Removed making a class out of Dice.  Nothing is being stored yet to need an object.

--- dice.py
+++ dice.py
@@ -4,28 +4,24 @@ import argparse
 import random
 
 
-class Dice:
-    def __init__(self):
-        pass
+def roll(critical_roll):
+    return random.randint(1, critical_roll)
 
-    def roll(self, critical_roll):
-        return random.randint(1, critical_roll)
+def full_hand(die_list):
+    total = [0]
+    for die in die_list:
+        (num_dice, critical_roll) = die.split('d')
+        (num_dice, critical_roll) = (int(num_dice), int(critical_roll))
 
-    def full_hand(self, die_list):
-        total = [0]
-        for die in die_list:
-            (num_dice, critical_roll) = die.split('d')
-            (num_dice, critical_roll) = (int(num_dice), int(critical_roll))
+        for y in range(0, num_dice):
+            face = roll(critical_roll)
+            total[0] += face
+            total.append(face)
 
-            for y in range(0, num_dice):
-                face = self.roll(critical_roll)
-                total[0] += face
-                total.append(face)
+    print("The roll is {}".format(total[1:]))
 
-        print("The roll is {}".format(total[1:]))
-
-        if len(total) > 2:
-            print("The total is {}".format(total[0]))
+    if len(total) > 2:
+        print("The total is {}".format(total[0]))
 
 
 if __name__ == '__main__':
@@ -34,6 +30,4 @@ if __name__ == '__main__':
                         help='Number and Type of Dice in the form NdX')
     args = parser.parse_args()
 
-    dice = Dice()
-
-    dice.full_hand(args.dice)
+    full_hand(args.dice)
