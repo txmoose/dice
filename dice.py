84ec252e0c046cf896a97022dda4df8068b74b6c
Kyle Brieden <kyle@brieden.org>
2015-04-26 06:03:54 -0500

    Added player name storage into class

--- dice.py
+++ dice.py
@@ -5,8 +5,8 @@ import random
 
 
 class Dice:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
 
     def roll(self, critical_roll):
         return random.randint(1, critical_roll)
@@ -22,18 +22,20 @@ class Dice:
                 total[0] += face
                 total.append(face)
 
-        print("The roll is {}".format(total[1:]))
+        print("{}'s roll is {}".format(self.name.capitalize(), total[1:]))
 
         if len(total) > 2:
-            print("The total is {}".format(total[0]))
+            print("{}'s total is {}".format(self.name.capitalize(), total[0]))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple dice roller')
     parser.add_argument('dice', nargs='+',
                         help='Number and Type of Dice in the form NdX')
+    parser.add_argument('-n', '--name', help='Name of roller', dest='name',
+                        default='Player', type=str)
     args = parser.parse_args()
 
-    dice = Dice()
+    dice = Dice(args.name)
 
     dice.full_hand(args.dice)
